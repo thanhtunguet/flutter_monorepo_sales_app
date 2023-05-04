4bd3c3b8fe6f24760b31a36036e6bde6328daf17
tungpt <thanhtung.uet@gmail.com>
2023-05-04 17:12:55 +0700

    update app title

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const SalesApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class SalesApp extends StatelessWidget {
+  const SalesApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Sales App',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Sales App'),
     );
   }
 }
